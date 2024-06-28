a74b3b0a1a48b6c9f865d8869a358bfde72ac6a0
Cloud <121166658+cloudedviper@users.noreply.github.com>
2024-06-28 03:49:17 -0500

    number array

--- index.js
+++ index.js
@@ -38,3 +38,10 @@ document.addEventListener('DOMContentLoaded', function() {
     app.appendChild(footer);
   });
   
+
+  const number = [1, 2, 3, 4, 5, 6]
+
+  const dubnumber = number.map(num => num * 2)
+
+  console.log(dubnumber)
+  
